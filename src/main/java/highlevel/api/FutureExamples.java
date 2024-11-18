e5f7975cb01ec7c56c03c0db785cfe3ea7a8d321
Efrem Dubrovin <efremdubrovin@deltasource.eu>
2024-11-18 22:09:13 +0200

    test countDownLatch

--- src/main/java/highlevel/api/FutureExamples.java
+++ src/main/java/highlevel/api/FutureExamples.java
@@ -10,6 +10,7 @@ public class FutureExamples {
 
     public static void main(String[] args) throws InterruptedException {
         cancelRunningTasksExample();
+
     }
 
     /**
---
+++ src/main/java/presentation/examples/TestHarness.java
@@ -0,0 +1,44 @@
+package presentation.examples;
+
+import java.util.concurrent.CountDownLatch;
+
+public class TestHarness {
+    public long timeTasks(int nThreads, final Runnable task) throws InterruptedException {
+
+        final CountDownLatch startGate = new CountDownLatch(1);
+        final CountDownLatch endGate = new CountDownLatch(nThreads);
+
+        for (int i = 0; i < nThreads; i++) {
+            Thread t = new Thread(() -> {
+                try {
+                    log("waiting players to start.");
+                    startGate.await();
+                    try {
+                        task.run();
+                    } finally {
+                        endGate.countDown();
+                        log("player has finished");
+                    }
+                } catch (InterruptedException ignored) {
+                    Thread.currentThread().interrupt();
+                }
+            });
+            t.start();
+        }
+        long start = System.nanoTime();
+        Thread.sleep(10);
+        startGate.countDown();
+        endGate.await();
+        long end = System.nanoTime();
+        return end - start;
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        TestHarness testHarness = new TestHarness();
+        testHarness.timeTasks(3, () -> log("task is executing"));
+    }
+
+    private static void log(String message) {
+        System.out.println(Thread.currentThread().getName() + " " + message);
+    }
+}
